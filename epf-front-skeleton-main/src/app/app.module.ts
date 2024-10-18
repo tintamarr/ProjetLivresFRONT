21c014b052e5c829cc9a39adc2b60142f9169835
ewarskotte <elea.warskotte@epfedu.fr>
2024-10-19 00:18:55 +0200

    Retrait des fichiers skelettes qui servaient à rien

--- epf-front-skeleton-main/src/app/app.module.ts
+++ epf-front-skeleton-main/src/app/app.module.ts
@@ -7,13 +7,9 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 import { NavbarComponent } from "components/navbar/navbar.component"
 import { MatListModule } from "@angular/material/list"
 import { HomeComponent } from "components/home/home.component"
-import { StudentsComponent } from "students/students.component"
-import { StudentDetailsComponent } from "students/student-details/student-details.component"
 import { FormsModule } from "@angular/forms"
 import { MatIconModule } from "@angular/material/icon"
 import { MatButtonModule } from "@angular/material/button"
-import { MajorsComponent } from "majors/majors.component"
-import { MajorStudentsComponent } from "majors/major-students/major-students.component"
 import { HttpClientModule } from "@angular/common/http";
 import { LivresComponent } from './components/livres/livres.component';
 import { CompteComponent } from './components/compte/compte.component'
@@ -23,10 +19,6 @@ import { CompteComponent } from './components/compte/compte.component'
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    StudentsComponent,
-    StudentDetailsComponent,
-    MajorsComponent,
-    MajorStudentsComponent,
     LivresComponent,
     CompteComponent,
   ],
