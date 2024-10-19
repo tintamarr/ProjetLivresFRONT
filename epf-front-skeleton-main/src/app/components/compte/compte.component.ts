5b633d279614e67ca2271bb2dc7b17c16b136b89
ewarskotte <elea.warskotte@epfedu.fr>
2024-10-20 00:13:14 +0200

    Debut page compte

--- epf-front-skeleton-main/src/app/components/compte/compte.component.ts
+++ epf-front-skeleton-main/src/app/components/compte/compte.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {UtilisateurService} from "../../services/utilisateur.service";
+import {Utilisateur} from "../../models/utilisateur.model";
 
 @Component({
   selector: 'epf-compte',
   templateUrl: './compte.component.html',
   styleUrls: ['./compte.component.scss']
 })
+
+//TODO : mettre des input à la place des noms pour pouvoir modifié et peut etre ne pas mettre dans un cadre
 export class CompteComponent implements OnInit {
 
-  constructor() { }
+  public utilisateurCompte!: Utilisateur;
+  public image_path ="";
+
+  constructor(private utilisateurService: UtilisateurService) { }
 
   ngOnInit(): void {
+    this.utilisateurService.getUtilisateurById(1).subscribe(utilisateur =>  {
+      this.utilisateurCompte = utilisateur;
+      this.image_path = "assets/pp/pp_humains_" + this.utilisateurCompte.photo_profil + ".png";
+      }
+    );
+
   }
 
 }
