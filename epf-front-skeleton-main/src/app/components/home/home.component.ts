0d44d3d805397e5b94f40fb32e2e01e3a3e2cc98
ewarskotte <elea.warskotte@epfedu.fr>
2024-10-28 22:14:40 +0100

    home-page livre en cours

--- epf-front-skeleton-main/src/app/components/home/home.component.ts
+++ epf-front-skeleton-main/src/app/components/home/home.component.ts
@@ -3,6 +3,7 @@ import { LivreService } from "../../services/livre.service";
 import { Livres } from "../../models/livres.model";
 import { debounceTime, distinctUntilChanged, Subject } from "rxjs";
 import { Router } from '@angular/router';
+import {LivresEnCoursService} from "../../services/livresencours.service";
 
 @Component({
   selector: "epf-home",
@@ -13,11 +14,17 @@ export class HomeComponent implements OnInit {
   listeLivres: Livres[] = [];
   searchResults: Livres[] = [];
   searchInput$ = new Subject<string>();
+  livreSelectionne !: Livres;
 
-  constructor(private livreService: LivreService, private router: Router) {}
+
+  //TODO : faire la barre de progression
+  //TODO : faire un bouton add pour pouvoir ajouter un livre
+
+  constructor(private livreService: LivreService, private livresEnCoursService : LivresEnCoursService, private router: Router) {}
 
   ngOnInit(): void {
     this.getLivre();
+    this.getLivreSelection();
     this.searchInput$.pipe(
       debounceTime(300),
       distinctUntilChanged()
@@ -33,6 +40,19 @@ export class HomeComponent implements OnInit {
   getLivre(): void {
     this.livreService.findAll().subscribe((livresReponse) => {
       this.listeLivres = livresReponse;
+    });
+  }
+
+  getLivreSelection(): void{
+    this.livresEnCoursService.findByUtilisateur(1).subscribe( livreencoursListe =>{
+      let livreencours;
+      if (livreencoursListe) {
+        livreencours = livreencoursListe[0];
+        const idLivre = livreencours.livre.idLivre;
+        if (idLivre !== undefined) {
+          this.livreService.findById(idLivre).subscribe(livre => this.livreSelectionne = livre);
+        }
+      }
     });
   }
 
@@ -52,4 +72,6 @@ export class HomeComponent implements OnInit {
     this.router.navigate(['/livre', livre.idLivre]);
     this.searchResults = [];
   }
+
+
 }
